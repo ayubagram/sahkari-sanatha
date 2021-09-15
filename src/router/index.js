c380a0a89fe6cb571823a921f2dd0f6d502bcab9
Ayub Alam <ayub@agramcloud.com>
2021-09-15 12:01:17 +0545

    services

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,9 @@ const routes = [
     component: () => import(/* webpackChunkName: "services" */ '../views/services/Services.vue'),
     children: [
       { path: 'mobile-banking', name: 'Mobile Banking', component: () => import(/* webpackChunkName: "mobile" */ '../views/services/Mobile.vue') },
-      { path: 'atm-card-service', name: 'ATM Card Service', component: () => import(/* webpackChunkName: "atm" */ '../views/services/ATM.vue') }
+      { path: 'atm-card-service', name: 'ATM Card Service', component: () => import(/* webpackChunkName: "atm" */ '../views/services/ATM.vue') },
+      { path: 'sms-banking', name: 'SMS Banking', component: () => import(/* webpackChunkName: "sms" */ '../views/services/SMS.vue') },
+      { path: 'remittance', name: 'Remittance', component: () => import(/* webpackChunkName: "remittance" */ '../views/services/Remittance.vue') }
     ]
   },
   { path: '/reports', name: 'Reports', component: () => import(/* webpackChunkName: "reports" */ '../views/Reports.vue') },
---
+++ src/views/services/Remittance.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <div class="mb-3" style="font-size: 25px;">Remittance</div>
+    <p>We provide various services for online money transfers. Our service is the best known, most secure and reliable online service. You can send or receive money within minute to/from other people around the globe. You pay the lowest fees in the market for your transfer. Recipients do not require a bank account to receive funds.</p>
+    <div class="mt-8 mb-2 primary--text" style="font-size: 25px;">Type of Money Transfer Services</div>
+    <ul>
+      <li v-for="(item, i) in items" :key="i"><p>{{ item }}</p></li>
+    </ul>
+  </v-container>  
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: ['Western Union', 'MoneyGram', 'IME Remit', 'Xpress Money', 'City Express', 'Prabhu Remit', 'Himal Remit', 'JME Remit', 'Nepal Remit', 'Samsara Remit', 'Muktinath Remit']  
+  })  
+}
+</script>
+
+<style scoped>
+ul li {
+  list-style: auto;
+  cursor: default;  
+}
+</style>
